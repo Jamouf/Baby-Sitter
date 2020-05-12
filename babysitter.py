8ce6463e11981f135054e3af4cb70d2564516f2a
Jamaal Ridley <Jamaal@jamaals-mbp.lan>
2020-05-12 19:51:23 -0400

    end time for after midnight

--- babysitter.py
+++ babysitter.py
@@ -1,4 +1,6 @@
 def hours_worked(start_time, end_time):  
     if start_time < 17:
         start_time = 17
+    if end_time not in range(12, 24 + 1):
+        end_time = end_time + 24
     return end_time - start_time
--- test_babysitter.py
+++ test_babysitter.py
@@ -5,4 +5,7 @@ def test_hours_worked():
      assert hours_worked(17,18) == 1
 
 def test_start_time():
-    assert hours_worked(16,18) == 1   
+    assert hours_worked(16,18) == 1
+
+def test_end_time():
+    assert hours_worked(23,0) == 1
