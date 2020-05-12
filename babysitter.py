73e954e97e0ec00cc22701aa714335de3040e37e
Jamaal Ridley <Jamaal@jamaals-mbp.lan>
2020-05-12 18:48:35 -0400

    can't work before 5pm

--- babysitter.py
+++ babysitter.py
@@ -1,4 +1,4 @@
-def hours_worked(start_time, end_time):
-    total_hours = end_time - start_time
-    return total_hours
-    
+def hours_worked(start_time, end_time):  
+    if start_time < 5:
+        start_time = 5
+    return end_time - start_time
