8410f313af6606b68ce7c4d98df30c3bf6322a88
Jamaal Ridley <Jamaal@jamaals-mbp.lan>
2020-05-12 20:02:12 -0400

    refactored start/end time

--- babysitter.py
+++ babysitter.py
@@ -1,6 +1,14 @@
-def hours_worked(start_time, end_time):  
+def hours_worked(start_time, end_time):
+    start_time = check_start_time(start_time)
+    end_time = check_end_time(end_time)  
+    return end_time - start_time
+
+def check_start_time(start_time):
     if start_time < 17:
         start_time = 17
+    return start_time
+
+def check_end_time(end_time):
     if end_time not in range(12, 24 + 1):
         end_time = end_time + 24
-    return end_time - start_time
+    return end_time
